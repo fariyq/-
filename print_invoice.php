987c4f6ca70198056b301da26a7261302b71cbba
fariyq <emranst101@gmail.com>
2025-03-05 01:57:39 +0800

    Create print_invoice.php

---
+++ print_invoice.php
@@ -0,0 +1,15 @@
+<?php
+include 'db.php';
+
+$id = $_GET['id'];
+$result = $conn->query("SELECT * FROM invoices WHERE id='$id'");
+
+if ($result->num_rows > 0) {
+    $invoice = $result->fetch_assoc();
+    echo "<h2>ইনভয়েস</h2>";
+    echo "<p>Phone: " . $invoice['phone'] . "</p>";
+    echo "<p>Total: " . $invoice['total'] . " টাকা</p>";
+} else {
+    echo "Invoice পাওয়া যায়নি!";
+}
+?>
